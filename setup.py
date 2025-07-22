591af6b8232e55bac2f494f9ebbe8266f2f33f73
Flynnt Knapp <bruce.stull@gmail.com>
2025-07-22 11:23:55 -0400

    In progress

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ setup(
     packages=find_packages(exclude=["project_test"]),
     include_package_data=True,
     description="A simple Django app for kat data. Imports into Kats application.",
-    long_description="README.md",
-    url="https://github.com/brucestull/kat-corner",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",  # Add this if README.md is Markdown
+    python_requires=">=3.7",  # Optional: helps restrict bad installs
+    url="https://github.com/brucestull/kats-kat-corner",
     author="Flynnt Knapp",
     author_email="FlynntKnapp@gmail.com",
     license="MIT",
